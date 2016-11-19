e81e3411218d942400f13fe7275238331f84c2d4
projetphpsen <simonvasseur91550@live.com>
2016-11-19 21:15:15 +0100

    ajout model vide

---
+++ Model/Channel.php
@@ -0,0 +1,35 @@
+<?php
+class Channel
+{
+    private $idChannel;
+    private $nameChannel;
+    private $nostreamer;
+    private $subscribersChannel;
+    private $descriptionChannel;
+    private $creationDateChannel;
+    private $viewsChannel;
+    public function __construct($nameChannel, $idNostreamer, $descriptionChannel)
+    {
+    }
+    public function getFromid($idNostreamer)
+    {
+    }
+    public function getOwnerName()
+    {
+    }
+    public function getSubscribersChannel()
+    {
+    }
+    public function getDescriptionChannel()
+    {
+    }
+    public function getCreationDateChannel()
+    {
+    }
+    public function getViewsChannel()
+    {
+    }
+    public function setDescriptionChannel($descriptionChannel)
+    {
+    }
+}
---
+++ Model/Video.php
@@ -0,0 +1,44 @@
+<?php
+class Video
+{
+    private $idVideo;
+    private $nameVideo;
+    private $channel;
+    private $dateVideo;
+    private $viewsVideo;
+    private $descriptionVideo;
+    private $rateVideo;
+    public function __construct($nameVideo, $channel, $descriptionVideo)
+    {
+    }
+    public function getName()
+    {
+    }
+    public function getChannel()
+    {
+    }
+    public function getDate()
+    {
+    }
+    public function getViews()
+    {
+    }
+    public function getDescription()
+    {
+    }
+    public function getRate()
+    {
+    }
+    public function setName($name)
+    {
+    }
+    public function addView()
+    {
+    }
+    public function addRate()
+    {
+    }
+    public function setDescription($description)
+    {
+    }
+}

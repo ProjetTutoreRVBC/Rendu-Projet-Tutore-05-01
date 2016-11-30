c3b378020c72bed1c3d6a16f03ae16acd3eb8dad
heliais77127 <heliais77127@hotmail.fr>
2016-11-30 07:26:59 -0500

    Finish Model

--- Model/Comment.php
+++ Model/Comment.php
@@ -12,20 +12,51 @@ class Comment
     private $rateComment;
     public function __construct($idPost, $idNostreamer, $idChannel, $idVideo, $titleComment, $messageComment)
     {
+      $this->idPost = $idPost;
+		  $this->idNostreamer = $idNostreamer;
+		  $this->idChannel = $idChannel;
+      $this->idVideo = $idVideo;
+      $this->titleComment = $titleComment;
+      $this->messageComment = $messageComment;
     }
+  
     public function getFromId($idPost, $idChannel, $idVideo)
     {
+      
     }
+  
     public function getTitleComment()
     {
+      return $this->titleComment;
     }
+  
     public function getMessageComment()
     {
+      return $this->messageComment;
     }
+  
     public function setTitleComment()
     {
+      $db = Database::getInstance($titleComment); //Why titleComment wasn't there before ?
+			$sql = "UPDATE Comment SET titleComment = :title WHERE idComment = :id";
+			$stmt = $db->prepare($sql);
+			$stmt->setFetchMode(PDO::FETCH_ASSOC);
+			$stmt->bindParam(':title',$titleComment);
+			$stmt->bindParam(':id',$idComment);
+			$stmt->execute();
+      $this->titleComment = $titleComment;
+			  
     }
+  
     public function setMessageComment($messageComment)
     {
+      $db = Database::getInstance();
+			$sql = "UPDATE Comment SET messageComment = :message WHERE idComment = :id";
+			$stmt = $db->prepare($sql);
+			$stmt->setFetchMode(PDO::FETCH_ASSOC);
+			$stmt->bindParam(':message',$messageComment);
+			$stmt->bindParam(':id',$idComment);
+			$stmt->execute();
+			$this->messageComment = $messageComment;
     }
 }
--- Model/Video.php
+++ Model/Video.php
@@ -10,35 +10,71 @@ class Video
     private $rateVideo;
     public function __construct($nameVideo, $channel, $descriptionVideo)
     {
+      $this->nameVideo = $nameVideo;
+      $this->channel = $channel;
+      $this->descriptionVideo = $descriptionVideo;
     }
+  
     public function getName()
     {
+      return $this->nameVideo;
     }
     public function getChannel()
     {
+      return $this->channel;
     }
+  
     public function getDate()
     {
+      return $this->dateVideo;
     }
+  
     public function getViews()
     {
+      return $this->viewsVideo;
     }
+  
     public function getDescription()
     {
+      return $this->descriptionVideo;
     }
+  
     public function getRate()
     {
+      return $this->rateVideo;
     }
+  
     public function setName($name)
     {
+      $db = Database::getInstance();
+			$sql = "UPDATE Video SET nameVideo = :name WHERE idVideo = :id";
+			$stmt = $db->prepare($sql);
+			$stmt->setFetchMode(PDO::FETCH_ASSOC);
+			$stmt->bindParam(':name',$name);
+			$stmt->bindParam(':id',$idVideo);
+			$stmt->execute();
+			$this->nameVideo = $nameVideo;
     }
+  
     public function addView()
     {
+      
     }
+  
     public function addRate()
     {
+      
     }
+  
     public function setDescription($description)
     {
+      $db = Database::getInstance();
+			$sql = "UPDATE Video SET descriptionVideo = :descr WHERE idVideo = :id";
+			$stmt = $db->prepare($sql);
+			$stmt->setFetchMode(PDO::FETCH_ASSOC);
+			$stmt->bindParam(':descr',$description);
+			$stmt->bindParam(':id',$idVideo);
+			$stmt->execute();
+			$this->descriptionVideo = $descriptionVideo;
     }
 }
